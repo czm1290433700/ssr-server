fb4b05c8c09f1651d87ad1170974394da0985c06
chenzhenmin <1290433700@qq.com>
2022-07-17 07:35:49 +0800

    fix: 修复webpack打包异常问题

--- webpack.base.js
+++ webpack.base.js
@@ -1,19 +1,13 @@
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-
 module.exports = {
   module: {
     rules: [
       {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: "babel-loader",
-            exclude: /node_modules/,
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        ],
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"],
+        },
       },
       {
         test: /\.(ts|tsx)?$/,
@@ -24,11 +18,5 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-    fallback: {
-      fs: false,
-      async_hooks: false,
-      net: false,
-    },
   },
-  plugins: [new NodePolyfillPlugin()],
 };
--- webpack.client.js
+++ webpack.client.js
@@ -4,9 +4,9 @@ const baseConfig = require("./webpack.base");
 
 module.exports = merge(baseConfig, {
   mode: "development",
-  entry: "/src/client/index.tsx",
+  entry: "./src/client/index.tsx",
   output: {
     filename: "index.js",
-    path: path.resolve(process.cwd(), "public"),
+    path: path.resolve(process.cwd(), "client_build"),
   },
 });
