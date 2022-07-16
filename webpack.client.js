4955be72de00134e8f5ed6962f54b8170ac9e58b
chenzhenmin <1290433700@qq.com>
2022-07-16 22:26:37 +0800

    feat:对浏览器相关新增打包

---
+++ webpack.client.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base");
+
+module.exports = merge(baseConfig, {
+  mode: "development",
+  entry: "/src/client/index.tsx",
+  output: {
+    filename: "index.js",
+    path: path.resolve(process.cwd(), "public"),
+  },
+});
