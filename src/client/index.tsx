737bc8a9893830b2bd99350c7f681d642bb4559c
陈祯民 <chenzhenmin@bytedance.com>
2022-12-11 13:10:43 +0800

    feat: 新增类型

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,22 +1,21 @@
-import { hydrateRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import router from "@/router";
-import { clientStore } from "@/store";
-import { Provider } from "react-redux";
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import router from '@/router';
+import { clientStore } from '@/store';
+import { Provider } from 'react-redux';
+import React from 'react';
 
-const Client = (): JSX.Element => {
-  return (
-    <Provider store={clientStore}>
-      <BrowserRouter>
-        <Routes>
-          {router?.map((item, index) => {
-            return <Route {...item} key={index} />;
-          })}
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  );
-};
+const Client = (): JSX.Element => (
+  <Provider store={clientStore}>
+    <BrowserRouter>
+      <Routes>
+        {router?.map((item, index) => (
+          <Route {...item} key={index} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 // 将事件处理加到ID为root的dom下
-hydrateRoot(document.getElementById("root") as Document | Element, <Client />);
+hydrateRoot(document.getElementById('root') as Document | Element, <Client />);
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,24 @@
-import { Fragment } from "react";
-import { Helmet } from "react-helmet";
+import React, { FC, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
-const Home = () => {
-  return (
-    <Fragment>
-      <Helmet>
-        <title>简易的服务器端渲染框架 - HOME</title>
-        <meta name="description" content="服务器端渲染框架"></meta>
-      </Helmet>
-      <div>
-        <h1>hello-ssr</h1>
-        <button
-          onClick={(): void => {
-            alert("hello-ssr");
-          }}
-        >
-          alert
-        </button>
-      </div>
-    </Fragment>
-  );
-};
+const Home: FC = () => (
+  <Fragment>
+    <Helmet>
+      <title>简易的服务器端渲染框架 - HOME</title>
+      <meta name="description" content="服务器端渲染框架" />
+    </Helmet>
+    <div>
+      <h1>hello-ssr</h1>
+      <button
+        type="button"
+        onClick={(): void => {
+          alert('hello-ssr');
+        }}
+      >
+        alert
+      </button>
+    </div>
+  </Fragment>
+);
 
 export default Home;
